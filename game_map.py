771dbb0af353f5b06f6347d482fce8c92539bac9
Josh Walker <joshwalker.oni@gmail.com>
2025-02-23 22:39:21 +0000

    FOV creation
    Added in working functions that generate a FOV based on given radius (8)
    currently and keeps track of explored map

--- game_map.py
+++ game_map.py
@@ -27,7 +27,9 @@ class GameMap:
         #fills self.tiles with floor tiles
         self.tiles= np.full((width, height), fill_value= tile_types.wall, order= 'F')
         
-        
+        #Add new np frames for currently visible and previously seen
+        self.visible=  np.full((width, height), fill_value= False, order= 'F')
+        self.explored=  np.full((width, height), fill_value= False, order= 'F')
         
 
     #Restricts player to avoid void
@@ -37,8 +39,31 @@ class GameMap:
     
     #Render map using Console tiles_rgb method
     def render(self, console: Console) -> None:
-        console.rgb[0: self.width, 0: self.height]= self.tiles['dark']
+        """
+        Render the map based on passable and iterable parameters
         
+        Visible:
+            Drawn with light colours
         
-        
+        Non visible but explored:
+            Draw in dark colours to show where we've seen
+            
+        Non visible and not explored:
+            Default to the SHROUD type
+        """
+
+        #Set the console to be conditionally drawn (np.select) based on condlist
+        console.rgb[0: self.width, 0: self.height]= np.select(
+            
+            #Check if tile is visibile or explored then uses corresponding value
+            condlist= [self.visible, self.explored],
+            choicelist= [self.tiles['light'], self.tiles['dark']],
+            
+            #If neither true in condlist sets default
+            default= tile_types.SHROUD
+        )
+
+
+
+
 
