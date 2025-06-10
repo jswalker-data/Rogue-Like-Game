362d2dd530958dd66325f13c6570fe63cd3ee651
Josh Walker <joshwalker.oni@gmail.com>
2025-06-10 16:34:11 +0100

    Rolled back TCDO and working here
    Remake the changes that were done in the v1 branch
    TODO: fix the screen size

--- input_handlers.py
+++ input_handlers.py
@@ -1,69 +1,58 @@
 # Type hinting that something can be set to None
 from __future__ import annotations
 
-
 from typing import Optional, TYPE_CHECKING
 
-import tcod.event
-from tcod import libtcodpy
+import tcod
 
 from actions import Action, BumpAction, EscapeAction, WaitAction
 
 if TYPE_CHECKING:
     from engine import Engine
 
+
 MOVE_KEYS = {
     # Arrow keys.
-    tcod.event.KeySym.UP: (0, -1),
-    tcod.event.KeySym.DOWN: (0, 1),
-    tcod.event.KeySym.LEFT: (-1, 0),
-    tcod.event.KeySym.RIGHT: (1, 0),
-    tcod.event.KeySym.HOME: (-1, -1),
-    tcod.event.KeySym.END: (-1, 1),
-    tcod.event.KeySym.PAGEUP: (1, -1),
-    tcod.event.KeySym.PAGEDOWN: (1, 1),
+    tcod.event.K_UP: (0, -1),
+    tcod.event.K_DOWN: (0, 1),
+    tcod.event.K_LEFT: (-1, 0),
+    tcod.event.K_RIGHT: (1, 0),
+    tcod.event.K_HOME: (-1, -1),
+    tcod.event.K_END: (-1, 1),
+    tcod.event.K_PAGEUP: (1, -1),
+    tcod.event.K_PAGEDOWN: (1, 1),
     # Numpad keys.
-    tcod.event.KeySym.KP_1: (-1, 1),
-    tcod.event.KeySym.KP_2: (0, 1),
-    tcod.event.KeySym.KP_3: (1, 1),
-    tcod.event.KeySym.KP_4: (-1, 0),
-    tcod.event.KeySym.KP_6: (1, 0),
-    tcod.event.KeySym.KP_7: (-1, -1),
-    tcod.event.KeySym.KP_8: (0, -1),
-    tcod.event.KeySym.KP_9: (1, -1),
+    tcod.event.K_KP_1: (-1, 1),
+    tcod.event.K_KP_2: (0, 1),
+    tcod.event.K_KP_3: (1, 1),
+    tcod.event.K_KP_4: (-1, 0),
+    tcod.event.K_KP_6: (1, 0),
+    tcod.event.K_KP_7: (-1, -1),
+    tcod.event.K_KP_8: (0, -1),
+    tcod.event.K_KP_9: (1, -1),
     # Vi keys.
-    tcod.event.KeySym.h: (-1, 0),
-    tcod.event.KeySym.j: (0, 1),
-    tcod.event.KeySym.k: (0, -1),
-    tcod.event.KeySym.l: (1, 0),
-    tcod.event.KeySym.y: (-1, -1),
-    tcod.event.KeySym.u: (1, -1),
-    tcod.event.KeySym.b: (-1, 1),
-    tcod.event.KeySym.n: (1, 1),
+    tcod.event.K_h: (-1, 0),
+    tcod.event.K_j: (0, 1),
+    tcod.event.K_k: (0, -1),
+    tcod.event.K_l: (1, 0),
+    tcod.event.K_y: (-1, -1),
+    tcod.event.K_u: (1, -1),
+    tcod.event.K_b: (-1, 1),
+    tcod.event.K_n: (1, 1),
 }
 
 WAIT_KEYS = {
-    tcod.event.KeySym.PERIOD,
-    tcod.event.KeySym.KP_5,
-    tcod.event.KeySym.CLEAR,
-}
-
-CURSOR_Y_KEYS = {
-    tcod.event.KeySym.UP: -1,
-    tcod.event.KeySym.DOWN: 1,
-    tcod.event.KeySym.PAGEUP: -10,
-    tcod.event.KeySym.PAGEDOWN: 10,
+    tcod.event.K_PERIOD,
+    tcod.event.K_KP_5,
+    tcod.event.K_CLEAR,
 }
 
 
-# EventHandler is a subclass of EventDispatch. Allows event sending
-# to proper methods
 class EventHandler(tcod.event.EventDispatch[Action]):
     def __init__(self, engine: Engine):
         self.engine = engine
 
     def handle_events(self, context: tcod.context.Context) -> None:
-        # Iterate through events and give knowledge of mouse position to the events
         for event in tcod.event.wait():
             context.convert_event(event)
             self.dispatch(event)
@@ -72,7 +61,6 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         if self.engine.game_map.in_bounds(event.tile.x, event.tile.y):
             self.engine.mouse_location = event.tile.x, event.tile.y
 
-    # Method of EventDispatch, when 'X' is hit (a quit event) we quit
     def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
         raise SystemExit()
 
@@ -82,26 +70,23 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
 class MainGameEventHandler(EventHandler):
     def handle_events(self, context: tcod.context.Context) -> None:
-        # Iterate through events and give knowledge of mouse position to the events
         for event in tcod.event.wait():
             context.convert_event(event)
+
             action = self.dispatch(event)
 
             if action is None:
                 continue
 
-            # Action -> Enemy action -> Update FOV
             action.perform()
-            self.engine.handle_enemy_turns()
-            self.engine.update_fov()
 
-    # Method will receive key presses and return Action class or None
+            self.engine.handle_enemy_turns()
+
+            self.engine.update_fov()  # Update the FOV before the players next action.
+
     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
-        # action holds whatever subclass of Action we choose or None
         action: Optional[Action] = None
 
-        # Holds the actual key pressed
-        # TODO: expand to modifiers e.g shift and alt
         key = event.sym
 
         player = self.engine.player
@@ -112,14 +97,12 @@ class MainGameEventHandler(EventHandler):
         elif key in WAIT_KEYS:
             action = WaitAction(player)
 
-        # Esc key returns EscapeAction
-        # TODO: make this a menu maybe?
-        elif key == tcod.event.KeySym.ESCAPE:
+        elif key == tcod.event.K_ESCAPE:
             action = EscapeAction(player)
-
-        elif key == tcod.event.KeySym.v:
+        elif key == tcod.event.K_v:
             self.engine.event_handler = HistoryViewer(self.engine)
 
+        # No valid key was pressed
         return action
 
 
@@ -138,15 +121,23 @@ class GameOverEventHandler(EventHandler):
 
         key = event.sym
 
-        if key == tcod.event.KeySym.ESCAPE:
+        if key == tcod.event.K_ESCAPE:
             action = EscapeAction(self.engine.player)
 
         # No valid key was pressed
         return action
 
 
+CURSOR_Y_KEYS = {
+    tcod.event.K_UP: -1,
+    tcod.event.K_DOWN: 1,
+    tcod.event.K_PAGEUP: -10,
+    tcod.event.K_PAGEDOWN: 10,
+}
+
+
 class HistoryViewer(EventHandler):
-    """Print log history on large window which can be navigated"""
+    """Print the history on a larger window which can be navigated."""
 
     def __init__(self, engine: Engine):
         super().__init__(engine)
@@ -154,23 +145,17 @@ class HistoryViewer(EventHandler):
         self.cursor = self.log_length - 1
 
     def on_render(self, console: tcod.Console) -> None:
-        # Draw the main state as the background
-        super().on_render(console)
+        super().on_render(console)  # Draw the main state as the background.
 
-        log_console = tcod.console.Console(console.width - 6, console.height - 6)
+        log_console = tcod.Console(console.width - 6, console.height - 6)
 
-        # Draw frame with custom banner
+        # Draw a frame with a custom banner title.
         log_console.draw_frame(0, 0, log_console.width, log_console.height)
         log_console.print_box(
-            0,
-            0,
-            log_console.width,
-            1,
-            "┤Message history├",
-            alignment=libtcodpy.CENTER,
+            0, 0, log_console.width, 1, "┤Message history├", alignment=tcod.CENTER
         )
 
-        # Render message log
+        # Render the message log using the cursor parameter.
         self.engine.message_log.render_messages(
             log_console,
             1,
@@ -182,30 +167,21 @@ class HistoryViewer(EventHandler):
         log_console.blit(console, 3, 3)
 
     def ev_keydown(self, event: tcod.event.KeyDown) -> None:
-        # Conditional movement
+        # Fancy conditional movement to make it feel right.
         if event.sym in CURSOR_Y_KEYS:
             adjust = CURSOR_Y_KEYS[event.sym]
-
-            # Only move from top to bottom on the edge
             if adjust < 0 and self.cursor == 0:
+                # Only move from the top to the bottom when you're on the edge.
                 self.cursor = self.log_length - 1
-
-            # Same but bottom to top
             elif adjust > 0 and self.cursor == self.log_length - 1:
+                # Same with bottom to top movement.
                 self.cursor = 0
-
-                # Else move while being bound to the bounds of the history log
             else:
+                # Otherwise move while staying clamped to the bounds of the history log.
                 self.cursor = max(0, min(self.cursor + adjust, self.log_length - 1))
-
-        # Move directly to top message
-        elif event.sym == tcod.event.KeySym.HOME:
-            self.cursor = 0
-
-        # Move to bottom message
-        elif event.sym == tcod.event.KeySym.END:
-            self.cursor = self.log_length - 1
-
-        # Any other key not above moves back to main game state
-        else:
+        elif event.sym == tcod.event.K_HOME:
+            self.cursor = 0  # Move directly to the top message.
+        elif event.sym == tcod.event.K_END:
+            self.cursor = self.log_length - 1  # Move directly to the last message.
+        else:  # Any other key moves back to the main game state.
             self.engine.event_handler = MainGameEventHandler(self.engine)
